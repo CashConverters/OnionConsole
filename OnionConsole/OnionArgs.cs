2b360f0d78b27f8ebaaa365b6bb47aa482dd8298
Derek Bingham <derek.bingham@readify.net>
2017-04-11 18:09:39 +0800

    Created Project structure

---
+++ OnionConsole/OnionArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDesk.Options;
+
+namespace OnionConsole
+{
+    public class OnionArgs
+    {
+        public string EntryAssembly { get; private set; }
+
+        private OptionSet options = new OptionSet
+        {
+           
+        };
+
+        public void ParseArgs(IEnumerable<string> args)
+        {
+
+                     
+        }
+    }
+}
