415fc308b2c25bd43e0afc723b9ba1148d801b89
Eric Winkler <eric@winkler.net.au>
2017-09-12 23:08:10 +0800

    Unhandled exceptions in the underlying service are now bubbled up

--- OnionConsole/CloudServiceWrapper.cs
+++ OnionConsole/CloudServiceWrapper.cs
@@ -68,16 +68,14 @@ namespace OnionConsole
 
         private void RunRole(Type roleType)
         {
-            try
-            {
-                // does this also apply to web roles??
-                if (!roleType.Name.Contains("Worker"))
+            // does this also apply to web roles??
+            if (!roleType.Name.Contains("Worker"))
             {
                 Console.WriteLine("Unable to run a non worker role");
                 return;
             }
-            
-            _workerRole = (RoleEntryPoint)Activator.CreateInstance(roleType);
+
+            _workerRole = (RoleEntryPoint) Activator.CreateInstance(roleType);
 
             if (!_workerRole.OnStart())
             {
@@ -85,27 +83,20 @@ namespace OnionConsole
                 return;
             }
 
-                Console.WriteLine("Role is starting for '{0}'", roleType);
-                _workerRole.Run();
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Unable to Run Role" + ex.GetBaseException());
-                _workerRole.OnStop();
-            }
+            Console.WriteLine("Role is starting for '{0}'", roleType);
+            _workerRole.Run();
         }
-
 
         public void StartCloudService()
         {
             var roleToRun = FindRoleEntryTypes()?.FirstOrDefault();
-            if (roleToRun != null)
+            if (roleToRun == null)
             {
-                Console.WriteLine($"Run Role Called for Role {roleToRun?.FullName}");
-                RunRole(FindRoleEntryTypes().FirstOrDefault());
-
+                throw new InvalidOperationException("Could not find role entry point");
             }
+
+            Console.WriteLine($"Run Role Called for Role {roleToRun.FullName}");
+            RunRole(roleToRun);
         }
     }
 }
